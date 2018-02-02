e356300f2bb0da19ac768ed399d00ff96cbc99bc
James Tharakan <James Tharakan>
2018-02-02 06:46:39 +0530

    stack class

---
+++ dataStructure/stack/MyStack.java
@@ -0,0 +1,38 @@
+package dataStructure.stack;
+
+public class MyStack {
+	
+	Object data [];
+	static int top = -1;
+	
+	public MyStack(int size){
+		data= new Object[size]; 
+	}
+	public boolean isEmpty(){
+		return top == -1;
+	}
+	
+	public void push(Object obj){
+		if(top < data.length-1){
+			top++;
+			data[top]=obj;
+			//System.out.println("Pushed :: "+obj);
+		}else{
+			//System.out.println("Stack over flow");
+		}
+	}
+	
+	public Object pop(){
+		Object obj= null;
+		
+		if(top != -1){
+			obj=data[top];
+			top--;
+			//System.out.println("Popped ::"+obj);
+		}else{
+			//System.out.println("Stack under flow");
+		}
+		
+		return obj;
+	}
+}
---
+++ puzzles/random/ExpressionCheck.java
@@ -0,0 +1,40 @@
+package puzzles.random;
+
+import dataStructure.stack.MyStack;
+
+public class ExpressionCheck {
+    
+    public static boolean isBalanced(String expression) {
+        boolean isBalanced= true;
+        MyStack stack = new MyStack(expression.length());
+        for(int i=0;i<expression.length();i++){
+            char n= expression.charAt(i);
+            
+            switch (n) {
+			case '(':
+					stack.push(')');
+				break;
+			case '{':
+					stack.push('}');
+				break;
+			case '[':
+				stack.push(']');
+				break;
+
+			default:
+				if(stack.isEmpty() || ((char)(stack.pop())!= n)){
+					return false;
+				}
+				break;
+			}
+        }
+        
+       return isBalanced; 
+    }
+  
+    public static void main(String[] args) {
+    	String expression = "{[(])}";
+           System.out.println(isBalanced(expression));
+        }
+    
+}
