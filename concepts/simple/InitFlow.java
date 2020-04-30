145e14721ba5d1358c7c47fe596ba8cb7a3a8730
James Tharakan <jtharakan@synamedia.com>
2020-04-30 22:15:33 +0530

    Started thread basics

--- concepts/simple/InitFlow.java
+++ concepts/simple/InitFlow.java
@@ -1,27 +1,31 @@
 package concepts.simple;
 
 public class InitFlow {
-	{
-		System.out.println("Initailazation block");
-	}
-	
-	static{
-		System.out.println("Static block");
-	}
-	
-	
-	
-	
-	InitFlow(){
-		System.out.println("Deafult constructor");
-	}
-	
-	InitFlow(int x){
-		System.out.println("parameter constructor");
-	}
-	
 	public static void main(String[] args) {
 		System.out.println("Main");
-		new InitFlow(1);
+		Flow.TryingToLoadClass = 1;
+		System.out.println("Accessed the variable , so that class will be loaded");
+		new Flow(1);
 	}
 }
+
+
+ class Flow{
+	 static int TryingToLoadClass =0;
+	 
+	 	static{
+			System.out.println("Static block");
+		}
+		
+	 	{
+			System.out.println("Initailazation block");
+		}
+		
+		Flow(){
+			System.out.println("Default constructor");
+		}
+		
+		Flow(int x){
+			System.out.println("parameter constructor");
+		}
+ }
