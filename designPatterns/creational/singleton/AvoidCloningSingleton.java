77742f3789de0a0a36a82a2121c7df0ddbdcc8fc
James Tharakan <jtharakan>
2022-03-17 23:45:47 +0530

    update some classes

---
+++ designPatterns/creational/singleton/AvoidCloningSingleton.java
@@ -0,0 +1,39 @@
+package designPatterns.creational.singleton;
+
+public class AvoidCloningSingleton {
+
+	public static void main(String[] args) {
+
+		cannotCloneMe instanceOne = cannotCloneMe.getInstance();
+
+		try {
+			instanceOne.clone();
+		} catch (CloneNotSupportedException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
+
+class cannotCloneMe {
+
+	private static cannotCloneMe instance = null;
+	
+	private cannotCloneMe() {
+		System.out.println("Created a single cannotCloneMe Object : " + this.hashCode());
+	}
+
+	public static cannotCloneMe getInstance() {
+		if (instance == null) {
+			instance = new cannotCloneMe();
+		}
+		return instance;
+	}
+
+	/*
+	 * Override and Just throw exception if someone tries to clone.
+	 */
+	protected Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException("I am a singleton class, you cannot clone me ");
+	}
+}
