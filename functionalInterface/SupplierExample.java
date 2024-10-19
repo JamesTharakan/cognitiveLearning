1dc0dcb3df62e02f129a06fadd1f6a2fce73730d
JamesTharakan <jamestharakan85@yahoo.com>
2024-10-19 15:43:13 +0530

    Supplier

---
+++ functionalInterface/SupplierExample.java
@@ -0,0 +1,14 @@
+package functionalInterface;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class SupplierExample {
+
+	public static void main(String[] args) {
+		Supplier <Integer> ran = ()-> new Random().nextInt(1000);
+		
+		System.out.println("Supplier sent info: "+ran.get());
+	}
+
+}
